a44181df250210c6c0284c02a894627d17416218
MaksymBohdan <maxymbohdan@gmail.com>
2020-09-20 22:28:42 +0300

    todo-structure

---
+++ src/views/Item.vue
@@ -0,0 +1,46 @@
+<template>
+  <li>
+    <span v-bind:class="{ completed: item.status }">
+      <input type="checkbox" v-on:change="item.status = !item.status" />
+      <strong>{{ idx }}</strong>
+      {{ item.name }}
+    </span>
+
+    <button class="rm" v-on:click="$emit('remove-item', item.id)">
+      &times;
+    </button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+    idx: { type: Number },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  border: 1px solid black;
+  padding: 5px 20px;
+  margin-bottom: 5px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.rm {
+  background-color: red;
+  color: white;
+  border-radius: 50%;
+  font-weight: bold;
+}
+
+.completed {
+  text-decoration: line-through;
+}
+</style>
---
+++ src/views/NewItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <form class="new-item" @submit.prevent="addItem">
+      <input type="text" placeholder="add item" v-model="text" />
+      <button type="submit">Create</button>
+    </form>
+    <br />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'new-item',
+  data() {
+    return {
+      text: '',
+    };
+  },
+  methods: {
+    addItem() {
+      console.log('form data', this.text);
+
+      if (this.text.trim()) {
+        const newItem = { id: Date.now(), name: this.text, status: false };
+
+        this.$emit('add-item', newItem);
+
+        this.text = '';
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.new-item {
+  border: 1px solid black;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+}
+</style>
